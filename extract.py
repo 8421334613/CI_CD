20b4e91e8a04ef6496b95d5dcdfe386d590801f4
sakhare1579 <bharatsakhare1579@gmail.com>
2025-12-04 11:02:26 +0530

    add data

--- extract.py
+++ extract.py
@@ -1,2 +1,13 @@
-print("load the data from mysql")
+print("extract the data from mysql")
+
+import pandas as  pd
+
+data={
+    id:[1,2,3,4,5],
+    "name":['a','b','c','d','e'],
+    "marks":[10,20,30,40,50]
+}
+
+data=pd.DataFrame(data)
+print(data)
 
